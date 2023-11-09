b137b086c98d54c7cc48b84731dd36f8f87d5e9a
kevinnathang <116236277+kevinnathang@users.noreply.github.com>
2023-11-09 11:26:18 +0900

    Add EventListeners for click and marks add up

--- script.js
+++ script.js
@@ -9,7 +9,21 @@ const Players = {
 
 const body = document.querySelector("body");
 
-for (let i = 0; i < Gameboard.gameboard.length; i++) {
-  let divSqr = document.createElement("div");
-  body.appendChild(divSqr);
-}
+(function () {
+  currentPlayer = Players.player1;
+
+  for (let i = 0; i < Gameboard.gameboard.length; i++) {
+    let divSqr = document.createElement("div");
+    divSqr.classList.add(`boardDiv${[i]}`);
+    divSqr.style.width = "50px";
+    divSqr.style.height = "50px";
+    divSqr.style.border = "thin solid black";
+    body.appendChild(divSqr);
+    divSqr.addEventListener("click", () => {
+      Gameboard.gameboard[i] = currentPlayer.mark;
+      currentPlayer =
+        currentPlayer === Players.player1 ? Players.player2 : Players.player1;
+      console.log(Gameboard.gameboard);
+    });
+  }
+})();
